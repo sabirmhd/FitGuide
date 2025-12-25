58de62b4b00dfc22d97ed8b9b9abf0443fa63f85
unknown <sabirmhd30@gmail.com>
2025-12-25 18:42:37 +0530

    final commit

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -96,10 +96,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Sabirmhd@123',
+        'HOST': 'db.rqcreuhrcrtpopidazza.supabase.co',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
