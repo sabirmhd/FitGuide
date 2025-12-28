abed1dc504b77ad794580e491732fe4a4c001a99
unknown <sabirmhd30@gmail.com>
2025-12-28 20:50:22 +0530

    frontend deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: import.meta.env.VITE_API_URL + '/api/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
