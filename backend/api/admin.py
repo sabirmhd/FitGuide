e3af758a17903bb1b6e105ed245280b6da058a98
unknown <sabirmhd30@gmail.com>
2026-01-20 23:56:27 +0530

    final

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from .models import (
-    Profile,
-    FoodLog,
-    WaterLog,
-    WeightLog,
-    ExerciseLog,
-    SleepLog
-)
+from .models import Profile, FoodLog, WaterLog, WeightLog, ExerciseLog, SleepLog
 
 # -------------------------
 # Profile
